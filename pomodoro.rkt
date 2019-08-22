a4d12ff701c095456189ccdc72149114c1249939
Antash Mishra <mishraantash34@gmail.com>
2019-08-22 16:51:10 +0545

    Updated version of pomodoro gui

--- pomodoro.rkt
+++ pomodoro.rkt
@@ -1,8 +1,9 @@
-#lang racket/gui
+#lang racket
+(require racket/gui)
 (define (current-posix-seconds)
   (/ (inexact->exact (current-inexact-milliseconds)) 1000))
-
-(define frame (new frame% [label "Timer"]))
+(define title "Timer")
+(define frame (new frame% [label title]))
 (define msg (new message% [parent frame]
                  [label "Press Start button to start the timer."]))
 
@@ -17,18 +18,27 @@
                                                (set! diff (- (current-seconds) begin))
                                                (send msg set-label (number->string diff)))]
                    [interval #f]))
+;text frame
+(define text-field-new-task
+  (new text-field%
+       (label "New task")
+       (parent frame)
+       (init-value "")))
 
 ; Start button
 (new button% [parent frame] [label "Start"]
      (callback (lambda (button event)
                  (set! begin (- (current-seconds) diff))
-                 (send timer start 10))))
-
-; Stop button
-(new button% [parent frame]
-     [label "Stop"]
-     (callback (lambda (button event)
-                 (send timer stop))))
+                 (send timer start 10)
+                 (let ((new-task-value (string-normalize-spaces
+                                          (send text-field-new-task get-value))))
+                     (if (not (equal? new-task-value ""))
+                         (begin
+                           (send list-box-task append new-task-value)
+                           (send (send text-field-new-task get-editor) erase))
+                         (begin
+                           (send text-field-new-task set-value "")
+                           (message-box title  "Task name is mandatory" frame)))))))
 
 ; Reset button
 (new button% [parent frame]
@@ -38,6 +48,41 @@
                  (set! begin (current-seconds))
                  (send msg set-label (number->string diff))
                  )))
+
+(define group-box-panel-task-ToDo
+  (new group-box-panel%
+       (parent frame)
+       (label "ToDo List")))
+
+(define list-box-task
+  (new list-box%
+       (label "")
+       (parent group-box-panel-task-ToDo)
+       (choices (list))
+       (style (list 'single))
+       (columns (list ""))))
+
+; Stop button
+(new button% [parent group-box-panel-task-ToDo]
+     [label "Stop"]
+     (callback (lambda (button event)
+                 (send timer stop)
+                  (let ((index (send list-box-task get-selection))
+                         (value (send list-box-task get-string-selection)))
+                     (send list-box-task-completed append value)
+                     (send list-box-task delete index)))))
+
+(define group-box-panel-task-completed
+  (new group-box-panel%
+       (parent frame)
+       (label "Task Completed")))
+(define list-box-task-completed
+  (new list-box%
+       (label "")
+       (parent group-box-panel-task-completed)
+       (choices (list))
+       (style (list 'single))
+       (columns (list ""))))
 
 ; show window
 (send frame show #t)
