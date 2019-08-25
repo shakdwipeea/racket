980a511214cc86867dcbc4bddd0b0beea3e2f542
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2019-08-25 23:04:33 +0530

    pomodor description

--- pomodoro.rkt
+++ pomodoro.rkt
@@ -1,5 +1,9 @@
 #lang racket
+
 (require racket/gui)
+(require racket/serialize)
+(require racket/stream)
+
 (define (current-posix-seconds)
   (/ (inexact->exact (current-inexact-milliseconds)) 1000))
 (define title "Timer")
@@ -11,6 +15,69 @@
 ; diff is (- (current-inexact-milliseconds) begin)
 (define begin 0)
 (define diff 0)
+
+
+;;;;;;;;;;;;;;
+;; Pomodoro ;;
+;;;;;;;;;;;;;;
+
+;; The Pomodoro Technique is a time management method developed by
+;; Francesco Cirillo in the late 1980s. The technique uses a
+;; timer to break down work into intervals, traditionally 25
+;; minutes in length, separated by short breaks. Each interval is
+;; known as a pomodoro, from the Italian word for 'tomato',
+;; after the tomato-shaped kitchen timer that Cirillo used as a
+;; university student
+
+;; The technique has been widely popularized by dozens of apps
+;; and websites providing timers and instructions. Closely
+;; related to concepts such as timeboxing and iterative
+;; and incremental development used in software design,
+;; the method has been adopted in pair programming contexts.
+
+
+;; There are six steps in the original technique:
+
+;; 1. Decide on the task to be done.
+;; 2. Set the pomodoro timer (traditionally to 25 minutes).
+;; 3. Work on the task.
+;; 4. End work when the timer rings
+;;    and put a checkmark on a piece of paper.
+;; 5. If you have fewer than four checkmarks,
+;;    take a short break (3–5 minutes),
+;;    then go to step 2.
+;; 6. After four pomodoros, take a longer break
+;;    (15–30 minutes),
+;;    reset your checkmark count to zero, then go to step 1.
+
+;; The stages of planning, tracking, recording, processing
+;; and visualizing are fundamental to the technique.
+;; In the planning phase, tasks are prioritized by recording
+;; them in a "To Do Today" list.
+;; This enables users to estimate the effort tasks require.
+;; As pomodoros are completed, they are recorded,
+;; adding to a sense of accomplishment and providing raw data
+;; for subsequent self-observation and improvement.
+
+
+;; For the purposes of the technique, a pomodoro is the interval
+;; of time spent working.
+;; After task completion, any time remaining in the Pomodoro
+;; is devoted to overlearning.
+;; Regular breaks are taken, aiding assimilation. A short
+;; (3–5 minutes) rest separates consecutive pomodoros.
+;; Four pomodoros form a set.
+;; A longer (15–30 minute) rest is taken between sets.
+
+;; A goal of the technique is to reduce the impact of internal
+;; and external interruptions on focus and flow.
+;; A pomodoro is indivisible when interrupted during a pomodoro,
+;; either the other activity must be recorded and
+;; postponed
+;; (using the inform – negotiate – schedule – call back strategy)
+;; https://lifehacker.com/productivity-101-a-primer-to-the-pomodoro-technique-1598992730
+;; or the pomodoro must be abandoned.
+
 
 ; Timer callback
 (define timer (new timer%
@@ -31,7 +98,7 @@
                  (set! begin (- (current-seconds) diff))
                  (send timer start 10)
                  (let ((new-task-value (string-normalize-spaces
-                                          (send text-field-new-task get-value))))
+                                        (send text-field-new-task get-value))))
                      (if (not (equal? new-task-value ""))
                          (begin
                            (send list-box-task append new-task-value)
